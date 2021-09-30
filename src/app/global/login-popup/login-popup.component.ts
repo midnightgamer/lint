681e720a342495d321ae714accc7e5784d30ccc3
Abhishek Waghela <abhi27051995@gmail.com>
2021-09-30 22:55:37 +0530

    Added Login functionality to login popup

--- src/app/global/login-popup/login-popup.component.ts
+++ src/app/global/login-popup/login-popup.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import { environment } from 'src/environments/environment';
 import {GlobalService} from "../../global.service";
+
+let open_window: any;
 
 @Component({
   selector: 'app-login-popup',
@@ -7,6 +10,7 @@ import {GlobalService} from "../../global.service";
   styleUrls: ['./login-popup.component.scss']
 })
 export class LoginPopupComponent implements OnInit {
+  cookie_interval: any;
 
   constructor(private global: GlobalService) {
   }
@@ -17,6 +21,23 @@ export class LoginPopupComponent implements OnInit {
 
   closePopup(): void {
     this.global.set_popup(false, 'login-popup')
+  }
+
+  loggedIn(): void {
+    this.cookie_interval = setInterval(()=>{
+      this.listen_cookie();
+    },100);
+    open_window = window.open(environment.sso_url, '_blank');
+  }
+
+  listen_cookie() {
+    if(document.cookie && document.cookie.split('iudx-ui-sso=')[1]) {
+      if(document.cookie.split('iudx-ui-sso=')[1].split(';')[0] == 'logged-in') {
+        clearInterval(this.cookie_interval);
+        open_window.close();
+        location.reload();
+      }
+    }
   }
 
 }
