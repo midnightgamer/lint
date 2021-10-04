693cc747080630562bd75661a7ac9d41a864d608
babai1203 <souptik.chatterjee7@gmail.com>
2021-10-04 10:44:00 +0530

    indentation fixes

--- src/app/request.interceptor.ts
+++ src/app/request.interceptor.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpEvent, HttpResponse, HttpRequest, HttpHandler } from '@angular/common/http';
+import { HttpInterceptor, HttpEvent, HttpRequest, HttpHandler } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
@@ -7,10 +7,10 @@ import { environment } from 'src/environments/environment';
 export class RequestInterceptor implements HttpInterceptor {
   intercept(httpRequest: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (httpRequest.url.indexOf(environment.auth_url) == -1 && httpRequest.url.indexOf(environment.keycloak.url) == -1) {
-        let Authorization = httpRequest.headers.get('Authorization');
-        if (Authorization) {
-            httpRequest = httpRequest.clone({ headers: httpRequest.headers.delete('Authorization', Authorization) });
-        }
+      let Authorization = httpRequest.headers.get('Authorization');
+      if (Authorization) {
+        httpRequest = httpRequest.clone({ headers: httpRequest.headers.delete('Authorization', Authorization) });
+      }
     }
     return next.handle(httpRequest);
   }
