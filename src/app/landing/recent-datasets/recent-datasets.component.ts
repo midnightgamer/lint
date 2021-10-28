3bcb70f9a8b8221cb60b2b0bcee3eb8821fd19e5
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2021-10-28 12:41:44 +0530

    test

--- src/app/landing/recent-datasets/recent-datasets.component.ts
+++ src/app/landing/recent-datasets/recent-datasets.component.ts
@@ -1,31 +1,28 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
-import { GlobalService } from 'src/app/global.service';
+import { Component, Input, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { GlobalService } from "src/app/global.service";
 
 @Component({
-  selector: 'app-recent-datasets',
-  templateUrl: './recent-datasets.component.html',
-  styleUrls: ['./recent-datasets.component.scss']
+	selector: "app-recent-datasets",
+	templateUrl: "./recent-datasets.component.html",
+	styleUrls: ["./recent-datasets.component.scss"]
 })
 export class RecentDatasetsComponent implements OnInit {
-  @Input() datasets: any = []
-  @Input() city: any;
-  @Input() summary: any;
+	@Input() datasets: any = [];
+	@Input() city: any;
+	@Input() summary: any;
 
-  constructor(private router: Router, private global: GlobalService) {
-  }
+	constructor(private router: Router, private global: GlobalService) {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {}
 
-  doSearch(): void {
-    let filters = this.global.get_default_filters();
-    this.global.set_filters(filters);
-    this.router.navigate(['/datasets']);
-  }
+          doSearch(): void {
+            const filters = this.global.get_default_filters();
+            this.global.set_filters(filters);
+            this.router.navigate(["/datasets"]);
+          }
 
-  goToSingleResource(dataset: any): void {
-    this.router.navigate(['/dataset', dataset.unique_id])
-  }
-
+	goToSingleResource(dataset: any): void {
+		this.router.navigate(["/dataset", dataset.unique_id]);
+	}
 }
