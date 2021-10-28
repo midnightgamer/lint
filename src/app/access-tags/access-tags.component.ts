cde6017b109b49d92ba5c5229f9cf3dc3c659c93
unknown <aakashsri.dev@gmail.com>
2021-10-28 17:34:42 +0530

    FIX: Error return

--- src/app/access-tags/access-tags.component.ts
+++ src/app/access-tags/access-tags.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
 	selector: "app-access-tags",
@@ -7,17 +7,21 @@ import { Component, Input, OnInit } from "@angular/core";
 })
 export class AccessTagsComponent implements OnInit {
 	verified = false;
-	fall: any;
-	name: any;
+	age: number;
+	name: string;
+	job: string;
 
 	constructor() {
 		this.name = "Aakash";
+		this.age = 24;
+		this.job = "UI Engineer";
 	}
 	ngOnInit(): void {
-		this.fall = true;
+		this.sayHello("hey aakash");
 	}
 
-	sayHello() {
-		const arg = "Shubham";
+	sayHello(params: string): string {
+		const name = params;
+		return name;
 	}
 }
